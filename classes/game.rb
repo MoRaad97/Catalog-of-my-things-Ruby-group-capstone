3aff9644057d595de3d070c0f4b2986dc006c13f
Zelalem1222 <zelalemfissha394@gmail.com>
2022-09-12 10:17:50 +0300

    Fix linter errors

--- classes/game.rb
+++ classes/game.rb
@@ -2,17 +2,16 @@ require_relative './item'
 require 'date'
 
 class Game < Item
+  attr_accessor :name, :multi_player, :last_played_at
 
-    attr_accessor :name, :multi_player, :last_played_at
+  def initialize(name, multi_player, last_played_at, _publish_date)
+    super(last_played_at)
+    @name = name
+    @multi_player = multi_player
+    @last_played_at = last_played_at
+  end
 
-    def initialize(name ,multi_player , last_played_at, publish_date)
-        super(last_played_at)
-        @name = name
-        @multi_player = multi_player
-        @last_played_at = last_played_at
-    end
-
-    def can_be_archived?
-      super && (Date.today.year - Date.parse(@last_played_at).year > 2)
-    end
+  def can_be_archived?
+    super && (Date.today.year - Date.parse(@last_played_at).year > 2)
+  end
 end
--- classes/movie.rb
+++ classes/movie.rb
@@ -1,16 +1,15 @@
 require_relative './item'
 
 class Movie < Item
- attr_accessor :silent, :name
-    def initialize(name , silent ,genre , publish_date)
-        super(publish_date)
-        @silent = silent
-        @name = name
-    end
+  attr_accessor :silent, :name
 
-    def can_be_archived?
-        super || silent 
-    end
+  def initialize(name, silent, _genre, publish_date)
+    super(publish_date)
+    @silent = silent
+    @name = name
+  end
 
+  def can_be_archived?
+    super || silent
+  end
 end
-
