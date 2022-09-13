be168c29d184462292935c224686ff06aa647026
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-09-13 12:01:53 +0300

    Create App class

---
+++ app.rb
@@ -0,0 +1,29 @@
+require_relative './helper.rb'
+
+class App
+  attr_reader :genres, :sources
+  attr_accessor :albums, :books
+
+  include BookHandle
+  include MusicAlbumHandle
+  include ReadData
+
+  def initialize
+    @books = []
+    read_books
+
+    @albums = []
+    read_music_album
+
+    @genres = []
+    read_genre
+    
+    @sources = []
+    read_source
+  end
+
+  
+
+
+
+end
